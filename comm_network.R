ba11f313bb89af3483e613de5390097fe464c270
Sith Jaisong <sith.jaisong@gmail.com>
2016-05-21 10:15:02 +0800

    upload script

---
+++ comm_network.R
@@ -0,0 +1,17 @@
+cluster.network <- function(graph){
+  
+  node <- as.character(V(graph)$name)
+  
+  Community <- cluster_optimal(graph, weights = abs(E(graph)$weight))
+  #Community <- page_rank(graph)
+  
+  prettyColors <- brewer.pal(n = 8, name = 'Set2')
+  
+  V(graph)$color <- prettyColors[membership(Community)]
+  
+  V(graph)$size <- 15
+  
+  E(graph)$width <- abs(E(graph)$weight)*10
+  
+  return(graph)
+}
