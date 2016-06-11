a304c3ca0f7cc5a59031632b6e3891557b10426b
Sith Jaisong <sith.jaisong@gmail.com>
2016-06-11 11:05:46 +0800

    correct comments

--- 02_comm_network.R
+++ 02_comm_network.R
@@ -1,10 +1,10 @@
 ########################################################################
 # title         : 02_comm_network.R
-# purpose       : detact clusters or communities in network graph
+# purpose       : detact clusters or communities in co-occurrence network; 
 # producer      : prepared by S. Jaisong (s.jaisong@irri.org);
 # last update   : in Los Baños, Laguna, PHL, Jun 2016;
-# inputs        : output as igraph object from 02_co_network_creation.R 
-# outputs       : can modified based on cluster algorith
+# inputs        : output as igraph object from 02_co_network_creation.R;
+# outputs       : can modified based on cluster algorithm
 ######################################################################
 
 library(RColorBrewer)
@@ -15,7 +15,7 @@ cluster.network <- function(graph){
   # detect comminities with maximizing mudularity
   community <- cluster_optimal(graph, weights = abs(E(graph)$weight))
   
-  # set colors for each community
+  # set colors from RcolorBrewer package for each community 
   prettyColors <- brewer.pal(n = 8, name = 'Set2')
   
   # define color to the community
